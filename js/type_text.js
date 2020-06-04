027acb18fa9a59595f3d23abced1e6756efdd334
wrench1815 <hardeepkumar1815@gmail.com>
2020-06-05 00:01:20 +0530

    update index css and js

--- js/type_text.js
+++ js/type_text.js
@@ -1,8 +1,18 @@
-/******************************************
+/****************************************************
  *  Author : Hardeep Kumar
  *  Created On : Thu Jun 04 2020
  *  File : type_text.js
- *******************************************/
+ *
+ *  Contains js Code for Text Typing Animations
+ ***************************************************/
 var typed = new Typed('#welcome_texts_anim', {
   stringsElement: '#welcome_texts',
+  smartBackspace: true,
+  typeSpeed: 20,
+  backSpeed: 20,
+  backDelay: 900,
+  loop: true,
+  loopCount: Infinity,
+  autoInsertCss: true,
+  shuffle: true,
 });
