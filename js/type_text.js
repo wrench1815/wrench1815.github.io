bc9fd79c37adffd66e5114d08fb26e65e2de5f1d
wrench1815 <hardeepkumar1815@gmail.com>
2020-06-04 22:34:41 +0530

    edit index added js

---
+++ js/type_text.js
@@ -0,0 +1,8 @@
+/******************************************
+ *  Author : Hardeep Kumar
+ *  Created On : Thu Jun 04 2020
+ *  File : type_text.js
+ *******************************************/
+var typed = new Typed('#welcome_texts_anim', {
+  stringsElement: '#welcome_texts',
+});
